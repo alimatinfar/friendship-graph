234bc2b493972cd9cd98654de90f00fc15c0b6e6
a.matinfar <alimatinfar1376@gmail.com>
2026-02-11 17:04:33 +0330

    [TASK]: added onNodesChange and onEdgesChange in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,41 @@
 import {
   ReactFlow,
   Background,
-  Controls,
+  Controls, useNodesState, useEdgesState,
 } from "@xyflow/react";
 import "@xyflow/react/dist/style.css";
 import PersonNode from "./components/Sections/PersonNode/PersonNode.tsx";
 import type {PersonNodeType} from "./components/Sections/PersonNode/PersonNode.types.ts";
 import createFakeNodesAndEdges from "./utils/createFakeNodesAndEdges.ts";
 
-const {nodes, edges} = createFakeNodesAndEdges()
+const {nodes: initialNodes, edges: initialEdges} = createFakeNodesAndEdges()
 
 const nodeTypes = {
   person: PersonNode,
 };
 
 export default function App() {
+
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  // useEffect(() => {
+  //   getLayoutElements(nodes, edges).then(({ nodes: layoutNodes }) => {
+  //     setNodes(layoutNodes);
+  //   });
+  // }, []);
+
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
       <ReactFlow<PersonNodeType>
         nodes={nodes}
         edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
         nodeTypes={nodeTypes}
         fitView
+        fitViewOptions={{ padding: 0.2 }}
+        minZoom={0.3}
+        maxZoom={2}
       >
         <Background />
         <Controls />
